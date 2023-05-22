98ff26603dd26a9a7cf69f816a335710b559b8bb
vanniowo <134260336+vanniowo@users.noreply.github.com>
2023-05-22 02:49:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click', function (){
-    alert('te quiero mucho, vamos uwu')
+    alert('te quiero mucho, vamos uwu');
 });
 
 const noBtn = document.querySelector('#noBtn')
 noBtn.addEventListener('mouseover', ()=>{
-  const randomX = Math.random()*100
-  const randomY = Math.random()*100
-  noBtn.style.position = 'absolute'
-  noBtn.style.top = randomY+'%'
-  noBtn.style.left = randomX+'%'
+  const randomX = Math.random()*100;
+  const randomY = Math.random()*100;
+  noBtn.style.position = 'absolute';
+  noBtn.style.top = randomY+'%';
+  noBtn.style.left = randomX+'%';
   noBtn.style.transform = `translate(-${randomX}%,
-  -${randomY}%)`
+  -${randomY}%)`;
 });
